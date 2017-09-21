0f746910e6133869d2f2a4ebdf2fef3651ea45f2
vinsol <basicallysaurish@gmail.com>
2017-09-21 13:58:12 +0530

    minor additions in eslint, aded configuration files

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+	render() {
+		return <div>Hello</div>;
+	}
+}
+
+class Goodbye extends React.Component {
+	render() {
+		return <div>Bye</div>;
+	}
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
