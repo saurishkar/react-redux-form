bc86541e48913aa5d3ea9cbf525e956bc6a63a64
vinsol <basicallysaurish@gmail.com>
2017-09-21 19:57:12 +0530

    minor changes

---
+++ src/components/posts_create.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+
+class PostsCreate extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state ={
+			success: ''
+		};
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(event) {
+		console.log('submitted');
+		
+		event.preventDefault();
+
+		this.setState({
+			success: 'Your Post has been submitted'
+		});
+	}
+
+	render() {
+		return (
+			<form className="form-group" onSubmit={this.handleSubmit}>
+				<Field name="title" component={}
+				<div>{this.state.success}</div>
+			</form>
+		);
+	}
+}
+
+export default reduxForm({
+	form: 'PostsNewForm'
+})(PostsCreate);
