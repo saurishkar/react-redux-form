6fb000f4d9e4cb642a882588ce1228f898426cc3
vinsol <basicallysaurish@gmail.com>
2017-09-21 14:09:09 +0530

    installed react route module and set up a basic route

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -20,8 +22,20 @@ class Goodbye extends React.Component {
 	}
 }
 
+class Home extends React.Component {
+	render() {
+		return <div>Welcome Home !</div>;
+	}
+}
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<BrowserRouter>
+		<div>
+			<Route path="/" component={Home} />
+			<Route path="/hello" component={Hello} />
+			<Route path="/goodbye" component={Goodbye} />
+		</div>
+		</BrowserRouter>
+	</Provider>
+	, document.querySelector('.container'));
